4510080f36ec58d5de8e31af7e8885dc03155bb8
Andy Fowlston <andy@fabbdev.com>
2023-02-23 13:52:12 +0000

    Build for use

--- __tests__/twig-extensions/alter-twig.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function addCustomExtension(\Twig_Environment &$env, $config) {
-  $env->addFunction(new \Twig_SimpleFunction('customTwigFunctionThatSaysWorld', function () {
-    return 'Custom World';
-  }));
-}
